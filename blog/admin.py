28061eda6fe4deb948bddb061d55a41573828f5d
Tkachev Valery <ebalaika1994@gmail.com>
2021-12-23 18:21:53 +0300

    create models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "author", "create_at", "id"]
+
+
+@admin.register(models.Reciept)
+class Reciept(admin.ModelAdmin):
+    list_display = ["name", "prep_time", "cook_time", "post"]
+
+
+admin.site.register(models.Category, MPTTModelAdmin)
+admin.site.register(models.Comment)
+admin.site.register(models.Tag)
