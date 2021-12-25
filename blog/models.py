4ff3c14f18d5d7555ab9a89d7bcf7b3c96f85a13
Tkachev Valery <ebalaika1994@gmail.com>
2021-12-25 14:20:48 +0300

    add post_detail and connect it

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from mptt.models import MPTTModel, TreeForeignKey
 
 
@@ -40,10 +41,20 @@ class Post(models.Model):
     category = models.ForeignKey(Category, related_name="post", on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag, related_name="post")
     create_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, default='')
 
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse("post_single", kwargs={"slug": self.category.slug, "post_slug": self.slug})
+
+
+    def get_recipes(self):
+        return self.recipe.all()
+
 
 
 class Recipe(models.Model):
